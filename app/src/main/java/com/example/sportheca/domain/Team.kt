b6f59c8b1730b0b176bf2b51441ff60ea31270c3
Rosiane <rosianeclemente1997@gmail.com>
2022-01-19 17:35:27 -0300

    adicionando classes

---
+++ app/src/main/java/com/example/sportheca/domain/Team.kt
@@ -0,0 +1,17 @@
+package com.example.sportheca.domain
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Team(
+    @SerializedName("nome")
+    val name: String,
+    @SerializedName("forca")
+    val stars: Int,
+    @SerializedName("imagem")
+    val image: String,
+    var score: Int?
+) : Parcelable
